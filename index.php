6e2b9051ccef54909b2305cd10a97c5e6ee12390
JeroenMolenaar2006 <9021323@student.zadkine.nl>
2024-09-09 11:42:20 +0200

    new repo for "herhaling"

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $firstName = "Jan";
+    $age = 25;
+    $height = 1.75;
+    $isStudent = true;
+    
+    echo $firstName;
+    echo "<br>";
+    echo $age;
+    echo "<br>";
+    echo $height;
+    echo "<br>";
+    echo $isStudent;
+
+
+    ?>
+</body>
+</html>
