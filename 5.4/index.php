68b1795903ded9e032fccbbc26cbf3595dbf03c3
JeroenMolenaar2006 <9021323@student.zadkine.nl>
2024-09-13 16:26:40 +0200

    geen BR'etjes lukte ff niet en ik wil door :P

---
+++ 5.4/index.php
@@ -0,0 +1,9 @@
+<?php
+$countries = array("Land" => "Duitsland", "Hoofdstad" => "Berlijn"
+                  ,"Land2" => "Frankrijk", "Hoofdstad2" => "Parijs"
+                  ,"Land3" => "Nederland", "Hoofdstad3" => "Amsterdam");
+
+echo "De hoofstad van" . $countries["Land"] . "is" . $countries["Hoofdstad"] ; 
+echo "De hoofstad van" . $countries["Land2"] . "is" . $countries["Hoofdstad2"];
+echo "De hoofstad van" . $countries["Land3"] . "is" . $countries["Hoofdstad3"];
+?>
