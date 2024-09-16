d7ee7f9aa81f56f183c6b444e6546ed617bd51fb
JeroenMolenaar2006 <9021323@student.zadkine.nl>
2024-09-16 10:52:26 +0200

    ik kom oprecht niet verder :(

--- 6.2/index.php
+++ 6.2/index.php
@@ -1,4 +1,12 @@
 <?php
 
+$prices = [100, 200, 300, 400, 500];
+While ($prices > 500) {
+    $newPrice = $prices + 50;
+    }
+
+echo "oorspronkelijke prijs:" . $prices;
+echo "nieuwe prijs:" . $newPrice;
+
 
 ?>
